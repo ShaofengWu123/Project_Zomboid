427b8eee90ff519d1a2de59a29a1f9462628078b
ShaofengWu123 <wsf123@mail.ustc.edu.cn>
2022-05-04 14:00:08 +0800

    Add cloud test settings with more mods

---
+++ cloud-test1_spawnregions.lua
@@ -0,0 +1,14 @@
+function SpawnRegions()
+	return {
+		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+		{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+		{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+		{ name = "carhouse", file = "media/maps/carhouse/spawnpoints.lua" },
+		{ name = "Xonics Mega Mall", file = "media/maps/Xonics Mega Mall/spawnpoints.lua" },
+		{ name = "Xonics Mega Mall Exterior", file = "media/maps/Xonics Mega Mall Exterior/spawnpoints.lua" },
+		{ name = "Xonics Mega Mall Interior", file = "media/maps/Xonics Mega Mall Interior/spawnpoints.lua" },
+		{ name = "Xonics Mega Mall Roof", file = "media/maps/Xonics Mega Mall Roof/spawnpoints.lua" },
+		{ name = "Chinatown", file = "media/maps/Chinatown/spawnpoints.lua" },
+	}
+end
